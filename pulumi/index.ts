dc6b4e14d6822837185549552e102a8cad35a442
yuya-morimoto <gyu2243dev@gmail.com>
2023-04-02 07:19:49 +0900

    update: index

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -1,18 +1,3 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as gcp from "@pulumi/gcp";
-import { constant } from "./src/constant";
+import * as flow from "./src/flow";
 
-const vpc = new gcp.compute.Network(`${constant.repositoryName}-main-vpc`, {
-  description: constant.description,
-  autoCreateSubnetworks: false,
-});
-
-const privateSubnet = new gcp.compute.Subnetwork(
-  `${constant.repositoryName}-private-subnet`,
-  {
-    ipCidrRange: "10.0.1.0/24",
-    region: constant.region["asia-northeast1"],
-    network: vpc.id,
-    description: constant.description,
-  }
-);
+flow.cloudBuildFlow();
