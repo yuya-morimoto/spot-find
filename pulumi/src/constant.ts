1bbc28dcd062ae6121bdb4321f1b366d81c268ad
yuya-morimoto <gyu2243dev@gmail.com>
2023-04-02 01:45:48 +0900

    update: vpc and subnet iac

---
+++ pulumi/src/constant.ts
@@ -0,0 +1,11 @@
+const repositoryName = "find-spot";
+const description = "managed pulumi";
+const region = {
+  "asia-northeast1": "asia-northeast1",
+};
+
+export const constant = {
+  description,
+  repositoryName,
+  region,
+};
