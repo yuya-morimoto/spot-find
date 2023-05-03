1fd38d427063bbf64cfd344dd55d33fae57407db
yuya-morimoto <gyu2243dev@gmail.com>
2023-05-03 16:31:01 +0900

    feat: nestjs setup#53

--- bff/zeus/src/app.module.ts
+++ bff/zeus/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CheckModule } from './check/check.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      driver: ApolloDriver,
+    }),
+    CheckModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
